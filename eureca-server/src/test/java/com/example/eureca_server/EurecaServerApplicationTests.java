1d5b08de44f5a93195ddac011cf89012d6c552e6
samuel-zeray <163921005+samuel-zeray@users.noreply.github.com>
2025-03-07 10:37:05 -0800

    Adding Eureca-server Micro-service

---
+++ eureca-server/src/test/java/com/example/eureca_server/EurecaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureca_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurecaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
